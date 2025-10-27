a3f624ae97fc9aef71986ac08c7be3288891705c
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-10-28 00:20:00 +0200

    Dark Mode UI. Header && Search Complete

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 document.querySelector('.theme-toggle').addEventListener('click', setTheme)
 
-
-
-
 function setTheme() {
     const root = document.documentElement;
+    const dayIcon = document.getElementById('day-icon')
+    const nightIcon = document.getElementById('night-icon')
+
+    if (root.className === 'dark') {
+        dayIcon.classList.add('icon-inactive')
+        nightIcon.classList.remove('icon-inactive')
+    } else {
+        dayIcon.classList.remove('icon-inactive')
+        nightIcon.classList.add('icon-inactive')
+    }
+
     const newTheme = root.className === 'dark' ? 'light' : 'dark';
     root.className = newTheme;
-  
 }
-

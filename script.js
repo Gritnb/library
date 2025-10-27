f23d4991120aac9ee8c118e49a8cbfe1d9e356d8
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-10-27 20:57:48 +0200

    Folder organisation - Template - Theme Switcher

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Works')
+document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+
+
+
+
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+  
+}
+

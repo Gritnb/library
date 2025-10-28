656fa116253e16ff669911a413d3e6ddc163d39f
GCarlott <181575691+Gritnb@users.noreply.github.com>
2025-10-28 19:31:25 +0200

    Basic form validation

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ import { setTheme } from './changeTheme.js'
 
 displayBooks()
 // Hooks
+const toggleTheme = document.getElementById('theme-toggle')
 const totalBooks = document.querySelector('.total')
 const readBooks = document.querySelector('.read')
 const unreadBooks = document.querySelector('.unread')
+const addNewBook = document.querySelector('.add-new-btn')
 
 // Listeners // UI Updates
-document.getElementById('theme-toggle').addEventListener('click', setTheme)
 totalBooks.textContent = library.length === 0 ? '0' : library.length
 readBooks.textContent = library.filter(book => book.status).length
 unreadBooks.textContent = library.filter(book => !book.status).length
+
+toggleTheme.addEventListener('click', setTheme)
+addNewBook.addEventListener('click', newForm)
 
 // Functions
 function Book(title, author, pages, status) {
@@ -27,3 +31,12 @@ function addBookToLibrary(title, author, pages, status) {
     book.id = crypto.randomUUID()
     library.push(book)
 }
+
+// function newForm() {
+//     const modal = document.querySelector('.form-container')
+//     modal.style.display = 'block'
+
+
+    
+// }
+
